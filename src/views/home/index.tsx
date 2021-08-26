ce74fce5db2e0849b82c0432f67ba776dcc27969
wjkang <ruoxieme@gmail.com>
2021-08-26 17:55:44 +0800

    update

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -11,12 +11,16 @@ export default defineComponent({
       // Let's turn this function declaration into a variable declaration.
       const code = `
 	  type b<T=any>={
-		coverRight?: boolean;
-		children?: React.ReactNode;
+		a:()=>void
+		// dsd
+		coverRight?: ffff;
+		/*
+		dsdsd
+		*/
+		children?: React.ReactNode.a.b
 		// dsdsd
 		//ewewe
 		contentPadding?: {a:string};
-		contentPadding2?: {b:string};
 	  };
 	  `;
 
@@ -50,69 +54,197 @@ export default defineComponent({
         return n.TSTypeAliasDeclaration.check(path.parent.node);
       };
 
+      const getType = (tsType?: string) => {
+        let type = "";
+        switch (tsType) {
+          case "TSAnyKeyword":
+            type = "any";
+            break;
+          case "TSNumberKeyword":
+            type = "number";
+            break;
+          case "TSStringKeyword":
+            type = "string";
+            break;
+          case "TSUnknownKeyword":
+            type = "any";
+            break;
+          case "TSBooleanKeyword":
+            type = "boolean";
+            break;
+          default:
+            break;
+        }
+        return type;
+      };
+
+      const getTSQualifiedNameString = (typeName: any) => {
+        let name: string[] = [];
+        if (n.TSQualifiedName.check(typeName)) {
+          if (typeName.right.type === "Identifier") {
+            name.push(typeName.right.name);
+          }
+          if (typeName.left.type === "Identifier") {
+            name.push(typeName.left.name);
+          }
+          if (typeName.left.type === "TSQualifiedName") {
+            name = name.concat(getTSQualifiedNameString(typeName.left));
+          }
+        }
+        return name.reverse().join(".");
+      };
+
+      const formatAst = (node: any) => {
+        if (n.TSTypeAliasDeclaration.check(node)) {
+          const name = node.id.name;
+          const typeParameters: typeof schema[0]["typeParameters"] = [];
+          const properties: typeof schema[0]["properties"] = [];
+          if (node.typeParameters) {
+            node.typeParameters.params.map((s) => {
+              let type = "";
+              switch (s.default?.type) {
+                case "TSAnyKeyword":
+                  type = "any";
+                  break;
+                case "TSNumberKeyword":
+                  type = "number";
+                  break;
+                case "TSStringKeyword":
+                  type = "string";
+                  break;
+                case "TSUnknownKeyword":
+                  type = "any";
+                  break;
+                case "TSBooleanKeyword":
+                  type = "boolean";
+                  break;
+                default:
+                  break;
+              }
+              typeParameters.push({
+                name: s.name,
+                default: type,
+              });
+            });
+          }
+          if (node.typeAnnotation.type === "TSTypeLiteral") {
+            node.typeAnnotation.members.map((s) => {
+              if (s.type === "TSPropertySignature") {
+                const property: typeof properties[0] = {
+                  optional: s.optional,
+                };
+                if (s.key.type === "Identifier") {
+                  property.name = s.key.name;
+                }
+                if (s.typeAnnotation?.type === "TSTypeAnnotation") {
+                  if (
+                    s.typeAnnotation.typeAnnotation.type === "TSTypeReference"
+                  ) {
+                    if (
+                      s.typeAnnotation.typeAnnotation.typeName.type ===
+                      "Identifier"
+                    ) {
+                      property.type =
+                        s.typeAnnotation.typeAnnotation.typeName.name;
+                    } else if (
+                      s.typeAnnotation.typeAnnotation.typeName.type ===
+                      "TSQualifiedName"
+                    ) {
+                      property.type = getTSQualifiedNameString(
+                        s.typeAnnotation.typeAnnotation.typeName
+                      );
+                    }
+                  } else {
+                    property.type = getType(
+                      s.typeAnnotation.typeAnnotation.type
+                    );
+                  }
+                }
+                if (s.comments) {
+                  const comments: string[] = [];
+                  s.comments.map((c) => {
+                    if (c.leading) {
+                      comments.push(c.value);
+                    }
+                  });
+                  property.comments = comments;
+                }
+                properties.push(property);
+              }
+            });
+          }
+          schema.push({
+            name: name,
+            typeParameters: typeParameters,
+            properties: properties,
+          });
+        }
+        return;
+      };
+
       const ast = recast.parse(code, { parser: typescriptParse });
       visit(ast, {
         visitExportNamedDeclaration(path) {
-          this.traverse(path);
+          console.log(path.node);
         },
         // 处理 type 声明的类型
         visitTSTypeAliasDeclaration(path) {
-          schema.push({});
-          this.traverse(path);
+          formatAst(path.node);
           console.log(JSON.stringify(schema, null, 4));
-        },
-        visitTSTypeLiteral(path) {
-          if (!isTopListeral(path)) {
-            schema.push({
-              name: getCurrentProperty().name,
-            });
-          }
           this.traverse(path);
         },
-        // 处理泛型参数
-        visitTSTypeParameter(path) {
-          if (n.TSTypeParameterDeclaration.check(path.parent.node)) {
-            if (!schema[schema.length - 1].typeParameters?.length) {
-              schema[schema.length - 1].typeParameters = [{}];
-            } else {
-              schema[schema.length - 1].typeParameters?.push({});
-            }
-            getCurrentTypeParameter().name = path.node.name;
-          }
-          this.traverse(path);
-        },
-        visitIdentifier(path) {
-          if (n.TSTypeAliasDeclaration.check(path.parent.node)) {
-            getCurrentSchema().name = path.node.name;
-          }
-          //   if (n.TSPropertySignature.check(path.parent.node)) {
-          //     getCurrentProperty().name = path.node.name;
-          //     console.log(getCurrentProperty(), path.node.name);
-          //   }
-          this.traverse(path);
-        },
-        visitTSPropertySignature(path) {
-          if (n.TSTypeLiteral.check(path.parent.node)) {
-            if (!schema[schema.length - 1].properties?.length) {
-              schema[schema.length - 1].properties = [{}];
-            } else {
-              schema[schema.length - 1].properties?.push({});
-            }
-            getCurrentProperty().optional = path.node.optional;
-            if (n.Identifier.check(path.node.key)) {
-              getCurrentProperty().name = path.node.key.name;
-            }
-          }
-          this.traverse(path);
-        },
-        // 处理 any 类型
-        visitTSAnyKeyword(path) {
-          // 在泛型参数中出现
-          if (n.TSTypeParameter.check(path.parent.node)) {
-            getCurrentTypeParameter().default = "any";
-          }
-          this.traverse(path);
-        },
+        // visitTSTypeLiteral(path) {
+        //   if (!isTopListeral(path)) {
+        //     schema.push({
+        //       name: getCurrentProperty().name,
+        //     });
+        //   }
+        //   this.traverse(path);
+        // },
+        // // 处理泛型参数
+        // visitTSTypeParameter(path) {
+        //   if (n.TSTypeParameterDeclaration.check(path.parent.node)) {
+        //     if (!schema[schema.length - 1].typeParameters?.length) {
+        //       schema[schema.length - 1].typeParameters = [{}];
+        //     } else {
+        //       schema[schema.length - 1].typeParameters?.push({});
+        //     }
+        //     getCurrentTypeParameter().name = path.node.name;
+        //   }
+        //   this.traverse(path);
+        // },
+        // visitIdentifier(path) {
+        //   if (n.TSTypeAliasDeclaration.check(path.parent.node)) {
+        //     getCurrentSchema().name = path.node.name;
+        //   }
+        //   //   if (n.TSPropertySignature.check(path.parent.node)) {
+        //   //     getCurrentProperty().name = path.node.name;
+        //   //     console.log(getCurrentProperty(), path.node.name);
+        //   //   }
+        //   this.traverse(path);
+        // },
+        // visitTSPropertySignature(path) {
+        //   if (n.TSTypeLiteral.check(path.parent.node)) {
+        //     if (!schema[schema.length - 1].properties?.length) {
+        //       schema[schema.length - 1].properties = [{}];
+        //     } else {
+        //       schema[schema.length - 1].properties?.push({});
+        //     }
+        //     getCurrentProperty().optional = path.node.optional;
+        //     if (n.Identifier.check(path.node.key)) {
+        //       getCurrentProperty().name = path.node.key.name;
+        //     }
+        //   }
+        //   this.traverse(path);
+        // },
+        // // 处理 any 类型
+        // visitTSAnyKeyword(path) {
+        //   // 在泛型参数中出现
+        //   if (n.TSTypeParameter.check(path.parent.node)) {
+        //     getCurrentTypeParameter().default = "any";
+        //   }
+        //   this.traverse(path);
+        // },
       });
     });
     return () => (
