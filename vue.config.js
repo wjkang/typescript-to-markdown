770436657e052844bfeea16739f9192a0f272e21
wjkang <ruoxieme@gmail.com>
2021-09-07 00:08:07 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-	css: {
-		extract: false,
-		requireModuleExtension: false,
-		loaderOptions: {
-			css: {
-				modules: {
-					localIdentName: '[local]_[hash:base64:5]',
-					auto: (resourcePath) => resourcePath.endsWith('.less'),
-				},
-				localsConvention: 'camelCaseOnly',
-			},
-		},
-	},
-	configureWebpack: {
-		optimization: {
-			splitChunks: false,
-		},
-	},
-	productionSourceMap: false,
-}
+  outputDir: "docs",
+  css: {
+    extract: false,
+    requireModuleExtension: false,
+    loaderOptions: {
+      css: {
+        modules: {
+          localIdentName: "[local]_[hash:base64:5]",
+          auto: (resourcePath) => resourcePath.endsWith(".less"),
+        },
+        localsConvention: "camelCaseOnly",
+      },
+    },
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: false,
+    },
+  },
+  productionSourceMap: false,
+};
